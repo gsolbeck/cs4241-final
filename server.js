854915bf6fe4ec7385a148509393e66900dbb42b
Gareth Solbeck <gsolbeck@gmail.com>
2016-11-18 20:34:20 -0500

    Add project description and server framework

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app);
+
+var options = {
+  port: 8080
+};
+
+
+app.use('/', express.static(__dirname + '/public'));
+
+http.listen(options.port, function () {
+  console.log('Game server listening on port ' + options.port);
+});
