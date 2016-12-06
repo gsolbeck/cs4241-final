9f0266aa1e71693540c8088525293015b0321c78
Gareth Solbeck <gsolbeck@gmail.com>
2016-12-06 18:01:41 -0500

    Add basic chat functionality using socket.io

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
   console.log('Document loaded');
+  var socket = io();
+
+  var messageBox = $('#message-box');
+  var submitButton = $('#submit-button');
+  var messageForm = $('#message-form');
+  var messageInput = $('#message-input');
+
+  messageForm.submit(function(event) {
+    event.preventDefault();
+    socket.emit('new message', {
+      user : 'anonymous',
+      message : messageInput.val()
+    });
+    messageInput.val('');
+  });
+
+  socket.on('update message', function(data) {
+    data.messages.forEach(function(msg) {
+      console.log('message received: ' + msg.message);
+      messageBox.append($('<div/>', {
+        text : msg.user + ': ' + msg.message
+      }));
+    });
+  });
+
 });
--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-var express = require('express'),
+var path = require('path'),
+    express = require('express'),
     app = express(),
-    http = require('http').Server(app);
+    http = require('http').Server(app)
+    io = require('socket.io')(http);
 
 var options = {
   port: process.env.PORT || 8080
 };
 
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('new message', function(data) {
+    console.log('message received: ' + data.message);
+    io.emit('update message', {
+      messages: [
+        {
+          user : data.user,
+          message : data.message
+        }
+      ]
+    });
+  });
+});
 
 http.listen(options.port, function () {
   console.log('Game server listening on port ' + options.port);
